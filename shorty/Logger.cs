bf47fbd22d93d2a81e0d10b764c416ebd701cae1
Duncan Cameron <dac31@hw.ac.uk>
2016-06-20 10:10:52 +0100

    initial commit + gitignore

---
+++ shorty/Logger.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+using Microsoft.Dafny;
+
+namespace shorty
+{
+    class Logger
+    {
+        TextWriter tw;
+        private List<Microsoft.Dafny.Program> programs;
+        private List<Tuple<string, int, int, float>> assertData = new List<Tuple<string, int, int, float>>();
+        private List<Tuple<string, int, int, float>> invariantData = new List<Tuple<string, int, int, float>>();
+        private int testNumbers = 1;
+
+        public Logger(TextWriter tw, List<Microsoft.Dafny.Program> programs)
+        {
+            this.tw = tw;
+            this.programs = programs;
+        }
+
+        public void RunTests()
+        {
+            tw.WriteLine("Logging data for {0} programs", programs.Count);
+            //Run all the tests
+            TestAssertRemoval();
+            tw.WriteLine("\n");
+            TestInvariantRemoval();
+            tw.WriteLine("\n");
+
+            //Log out extra statistics
+
+            //assertions
+            int totalAssertions = 0;
+            int totalRemovedAssertions = 0;
+            foreach (var data in assertData) {
+                totalAssertions += data.Item2;
+                totalRemovedAssertions += data.Item3;
+            }
+            float percentageRemoved = (totalRemovedAssertions / totalAssertions) * 100;
+            tw.WriteLine("Percentage of assertions removed: {0}", percentageRemoved);
+        }
+
+        public void TestAssertRemoval()
+        {
+            tw.WriteLine("Assert Removal");
+            tw.WriteLine("Program Name, Asserts Before, Asserts Removed, Average Execution Time(ms)");
+            
+            foreach (Microsoft.Dafny.Program program in programs) {
+                Console.WriteLine("\nRemoving Asserts from {0}", program.FullName);
+                int assertsBefore = 0;
+                int assertsRemoved = 0;
+                float averageExecutionTime = 0;
+                        
+                // Run each test and add up the time of each one so the average can be found
+                for (int i = 0; i < testNumbers; i++) {
+                    //@TODO: Copy the program so it actually goes through the whole process each time and can resolve properly
+                    Shorty shorty = new Shorty(program, Shorty.Mode.Singular);
+
+                    if (i == 0) {
+                        //Find out how many asserts were in the program before the removal - only do on first run
+                        foreach (Statement stmt in shorty.asserts.Keys) {
+                            assertsBefore += shorty.asserts[stmt].Count;
+                        }
+                    }
+
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    List<AssertStmt> asserts = shorty.FindUnnecessaryAsserts();
+                    sw.Stop();
+
+                    averageExecutionTime += sw.ElapsedMilliseconds;
+
+                    //Gather all other needed information in the first run
+                    if (i == 0) {
+                        assertsRemoved = asserts.Count;
+                    }
+                }
+
+                //Calculate average execution time and
+                averageExecutionTime = averageExecutionTime/testNumbers;
+                assertData.Add(new Tuple<string, int, int, float>(program.FullName, assertsBefore, assertsRemoved, averageExecutionTime));
+                tw.WriteLine("{0}, {1}, {2}, {3}", program.FullName, assertsBefore, assertsRemoved, averageExecutionTime);
+            }
+        }
+
+        public void TestInvariantRemoval()
+        {
+            tw.WriteLine("Invariant Removal");
+            tw.WriteLine("Program Name, Invariants Before, Invariants Removed, Average Execution Time(ms)");
+
+            foreach (Microsoft.Dafny.Program program in programs) {
+                Console.WriteLine("\nRemoving invariants from {0}", program.FullName);
+                int invariantsBefore = 0;
+                int invariantsRemoved = 0;
+                float averageExecutionTime = 0;
+
+                for (int i = 0; i < testNumbers; i++) {
+                    //@TODO: Copy the program so it actually goes through the whole process each time and can resolve properly
+                    Shorty shorty = new Shorty(program, Shorty.Mode.Singular);
+
+                    if (i == 0) {
+                        //Find out how many invariants were in the program before the removal - only do on first run
+                        foreach (LoopStmt stmt in shorty.invariants.Keys) {
+                            invariantsBefore += shorty.invariants[stmt].Count;
+                        }
+                    }
+
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    List<MaybeFreeExpression> invariants = shorty.FindRemovableInvariants();
+                    sw.Stop();
+
+                    averageExecutionTime += sw.ElapsedMilliseconds;
+
+                    //Gather all other needed information in the first run
+                    if (i == 0) {
+                        invariantsRemoved = invariants.Count;
+                    }
+                }
+
+                //Calculate average execution time and
+                averageExecutionTime = averageExecutionTime/testNumbers;
+                invariantData.Add(new Tuple<string, int, int, float>(program.FullName, invariantsBefore, invariantsRemoved,
+                    averageExecutionTime));
+                tw.WriteLine("{0}, {1}, {2}, {3}", program.FullName, invariantsBefore, invariantsRemoved, averageExecutionTime);
+            }
+        }
+    }
+}
---
+++ shorty/ShortyMain.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Dafny;
+using Bpl = Microsoft.Boogie;
+
+namespace shorty
+{
+    class ShortyMain
+    {
+        static Bpl.OutputPrinter printer;
+        //private Printer printer;
+
+        static void RunLogger(List<Program> programs)
+        {
+            using (TextWriter writer = File.CreateText("H:\\dafny\\test.csv")) {
+                Logger logger = new Logger(writer, programs);
+                logger.RunTests();
+            }
+
+        }
+
+        private static void Main(string[] args)
+        {
+            Contract.Requires(args != null);
+            //Setup environment
+            DafnyOptions.Install(new DafnyOptions());
+            Bpl.CommandLineOptions.Clo.Z3ExecutablePath = "H:\\dafny\\repos\\tacny\\tacny\\Binaries\\z3.exe";
+            Bpl.CommandLineOptions.Clo.ApplyDefaultOptions();
+            Bpl.CommandLineOptions.Clo.VerifySnapshots = 1;
+            printer = new Bpl.ConsolePrinter();
+            //printer.
+            Bpl.ExecutionEngine.printer = printer;
+
+            //string[ filename = new string[args.Length];
+            List<Program> dafnyPrograms = new List<Program>();
+
+            List<string> fileNames = new List<string>();
+
+            //Check for * directories
+            foreach (string fileName in args) {
+                if (fileName.EndsWith("\\*") || fileName.EndsWith("/*")) {
+                    string[] newFileNames = Directory.GetFiles(fileName.Substring(0, fileName.Length - 1));
+                    foreach (string newFileName in newFileNames) {
+                        if(newFileName.EndsWith(".dfy"))
+                            fileNames.Add(newFileName);
+                    }
+                }
+                else if (fileName.EndsWith(".dfy")) {
+                    fileNames.Add(fileName);
+                }
+                else {
+                    Console.WriteLine(fileName + " not recognised.");
+                }
+            }
+
+            foreach(string fileName in fileNames) {
+
+                Console.WriteLine("Filename: " + fileName);
+
+                if (!fileName.EndsWith(".dfy"))
+                {
+                    Console.WriteLine("Error: file must be a .dfy file");
+                    continue;
+                }
+
+
+                //create program from file
+                int nameStart = fileName.LastIndexOf('\\') + 1;
+                string programName = fileName.Substring(nameStart, fileName.Length-nameStart);
+
+                ModuleDecl module = new LiteralModuleDecl(new DefaultModuleDecl(), null);
+                var builtIns = new BuiltIns();
+                Parser.Parse(fileName, module, builtIns, new Errors());
+
+                dafnyPrograms.Add(new Program(programName, module, builtIns));
+
+            }
+
+            Console.WriteLine("1: standard run\n2: run logger");
+            string input = Console.ReadLine();
+            int ans;
+            if(!Int32.TryParse(input, out ans)) {
+                return;
+            }
+            else if (ans < 1 || ans > 2) {
+                return;
+            }
+
+            if (ans == 2) {
+                RunLogger(dafnyPrograms);
+                return;
+            }
+
+
+
+            //setup shorty and test the files
+            Dictionary<Program, List<AssertStmt>> removeableAsserts = new Dictionary<Program, List<AssertStmt>>();
+            Dictionary<Program, List<MaybeFreeExpression>> removeableInvariants = new Dictionary<Program, List<MaybeFreeExpression>>();
+            Dictionary<Program, List<Expression>> removeableDecreases = new Dictionary<Program, List<Expression>>();
+            Dictionary<Program, List<UpdateStmt>> removableLemmaCalls = new Dictionary<Program, List<UpdateStmt>>();
+            List<Program> failedInitialValidationPrograms = new List<Program>();
+            List<Program> failedAssertRemovalPrograms = new List<Program>();
+
+            // Time analysis
+            Dictionary<Program, long> times = new Dictionary<Program,long>();
+            Stopwatch sw = new Stopwatch();
+
+            // Run all the programs
+            foreach (Program program in dafnyPrograms) {
+                sw.Reset();
+                sw.Start();
+                Shorty.Mode mode = Shorty.Mode.Singular;
+                var shorty = new Shorty(program, mode);
+//                shorty.PrintAsserts();
+                Console.WriteLine(program.FullName);
+                if (!shorty.IsProgramValid()) {
+                    Console.WriteLine("Initial failed");
+                    failedInitialValidationPrograms.Add(program);
+                    break;
+                }
+
+                Console.WriteLine("Finding unnecesary asserts");
+                List<AssertStmt> asserts = shorty.FindUnnecessaryAsserts();
+                if (asserts == null) {
+                    Console.WriteLine("Finding unnecessary asserts failed");
+                    failedAssertRemovalPrograms.Add(program);
+                    break;
+                }
+
+                Console.WriteLine("Finding unnecessary loop invariants");
+                List<MaybeFreeExpression> invariants = shorty.FindRemovableInvariants();
+
+                if (invariants == null) {
+                    Console.WriteLine("Finding invariants failed");
+                    break;
+                }
+
+                Console.WriteLine("Finding unnecessary loop invariants");
+                List<Expression> decreases = shorty.FindRemoveableDecreases();
+                if (decreases == null)
+                {
+                    Console.WriteLine("Finding decreases failed");
+                    break;
+                }
+
+                Console.WriteLine("Finding unnecessary lemma calls");
+                List<UpdateStmt> lemmaCalls = shorty.FindRemovableLemmaCalls();
+                if (lemmaCalls == null) {
+                    Console.WriteLine("Finding lemma calls failed");
+                    break;
+                }
+
+                if (!shorty.IsProgramValid())
+                {
+                    Console.WriteLine("\n\n at end and somehow failed!!!!\n\n");
+                    failedInitialValidationPrograms.Add(program);
+                }
+
+
+                //shorty.RemoveAutoGeneratedDecreases();
+
+                removeableAsserts.Add(program, asserts);
+                removeableInvariants.Add(program, invariants);
+                removeableDecreases.Add(program, decreases);
+                removableLemmaCalls.Add(program, lemmaCalls);
+                sw.Stop();
+                times.Add(program, sw.ElapsedMilliseconds);
+                using (TextWriter writer = File.CreateText("H:\\dafny\\programs\\shortied-" + program.FullName)) {
+                    shorty.PrintProgram(writer);
+                }
+
+            }
+
+            Console.WriteLine("\n\nRemoveable asserts and invariants for each program:");
+            foreach (Program program in removeableAsserts.Keys) {
+                float time = times[program]/1000f;
+                Console.WriteLine(program.FullName + " - running time: " + time +"s " );
+                string asserts = "";
+                foreach (AssertStmt assert in removeableAsserts[program]) {
+                    asserts = (asserts + " (" + assert.Tok.line + "," + assert.Tok.col + "),");
+                }
+                if(removeableAsserts[program].Count > 0)
+                    asserts = "Asserts:" + asserts.Substring(0, asserts.Length-1); //remove last comma
+                else {
+                    asserts = "No asserts were removed.";
+                }
+                string invariants = "";
+                foreach (MaybeFreeExpression invariant in removeableInvariants[program]) {
+                    invariants = invariants + " (" + invariant.E.tok.line + "," + invariant.E.tok.col + "),";
+                }
+
+                if (removeableInvariants[program].Count > 0)
+                    invariants = "Invariants:" + invariants.Substring(0, invariants.Length - 1);
+                else
+                    invariants = "No invariants were removed";
+
+                string decreaseses = "";
+                foreach (Expression decreases in removeableDecreases[program]) {
+                    decreaseses = decreaseses + " (" + decreases.tok.line + "," + decreases.tok.col + "),";
+                }
+
+                if (removeableDecreases[program].Count > 0)
+                    decreaseses = "Decreaseses:" + decreaseses.Substring(0, decreaseses.Length - 1);
+                else
+                    decreaseses = "No decreaseses were removed";
+
+                string lemmaCalls = "";
+                foreach (UpdateStmt update in removableLemmaCalls[program]) {
+                    lemmaCalls = lemmaCalls + " (" + update.Tok.line + "," + update.Tok.col + "),";
+                }
+
+                if (removableLemmaCalls[program].Count > 0)
+                    lemmaCalls = "LemmaCalls:" + lemmaCalls.Substring(0, lemmaCalls.Length - 1);
+                else {
+                    lemmaCalls = "No lemma calls were removed";
+                }
+                
+                Console.WriteLine(asserts + "\n");
+                Console.WriteLine(invariants + "\n");
+                Console.WriteLine(decreaseses + "\n");
+                Console.WriteLine(lemmaCalls + "\n");
+            }
+
+            Console.WriteLine("\nPrograms that failed initial verification:");
+            foreach (Program program in failedInitialValidationPrograms) {
+                Console.Write(program.FullName + " | ");
+            }
+
+            Console.WriteLine("\nPrograms that failed assert removal:");
+            foreach (Program program in failedAssertRemovalPrograms) {
+                Console.Write(program.FullName + " | ");
+            }
+            Console.WriteLine("\nElapsed Time: " + (DateTime.Now - Process.GetCurrentProcess().StartTime));
+
+            Console.Read();
+        }
+    }
+}
